49201a457889f73e42b40be4765532950dbd3051
tadimy <toleranhink@gmail.com>
2015-11-03 00:35:28 +0800

    init ng.vaivei.com

---
+++ src/scripts/app.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Angular2 on 15-11-2.
+ */
+import {Component, bootstrap} from "angular2/angular2";
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+class AppComponent {
+}
+bootstrap(AppComponent);
